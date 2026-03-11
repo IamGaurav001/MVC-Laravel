4b54f8f1200d6613a0bf451e25fb2079671464d0
IamGaurav001 <hellogauravdubey@gmail.com>
2026-03-11 16:03:29 +0530

    Basic Routing

--- test-laravel-1/routes/web.php
+++ test-laravel-1/routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Basic Routing
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/display', function () {
+    for($i=1; $i<5; $i++)
+        echo "$i"."<br>";
+});
+
+$courseList = ["php", "laravel", "mysql", "html", "css"];
+
+Route::get('/course', function () use ($courseList) {
+    foreach ($courseList as $c) {
+        echo $c . "<br>";
+    }
+});
+
+//Req Routing
+Route::get('/user/{name}', function ($name) {
+    return "Hello $name";
 });
